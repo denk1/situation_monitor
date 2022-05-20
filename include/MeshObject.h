ac299618e8e53f9504ee4ab122849a15b4621dd3
Your Name <tu214@yandex.ru>
2022-05-20 20:27:29 +0300

    the obstacle map for networking

--- include/MeshObject.h
+++ include/MeshObject.h
@@ -9,14 +9,14 @@ class SituationMonitor;
 class MeshObject {
 public:
     MeshObject(SituationMonitor* ptrSituationMonitor);
-    typedef std::vector<Ogre::Matrix4> vec_mat4;
+    typedef std::map<size_t, Ogre::Matrix4> map_mat4;
     void set_str_buff(const std::string& str_buff);
     void convert_to_scene_obj();
     void create_scene_nodes();
     
 private:
     std::string buff_scene_;
-    vec_mat4 buff_vec_mat4_;
+    map_mat4 buff_map_mat4_;
     SituationMonitor* mPtrSituationMonitor_;
     Ogre::Vector3 own_position_; 
 
--- src/MeshObject.cpp
+++ src/MeshObject.cpp
@@ -15,22 +15,33 @@ void MeshObject::set_str_buff(const std::string& str_buff) {
 
 void MeshObject::convert_to_scene_obj() {
     byte s_in_bytes[8];
+    byte id_in_bytes[8];
     byte* byte_mat4;
     Ogre::Real mat4[16];
     byte_mat4 = reinterpret_cast<byte*>(mat4);
     
     std::copy(buff_scene_.begin() + 2, buff_scene_.begin() + 10, s_in_bytes);
     const size_t* s = reinterpret_cast<const size_t*>(s_in_bytes);
-    buff_vec_mat4_.clear();
+    buff_map_mat4_.clear();
     for(size_t i = 0; i < *s; i++) {
-        size_t size_of_mat4 = i * 64;
-        std::copy(buff_scene_.begin() + 10 + size_of_mat4, buff_scene_.begin() + 64 + size_of_mat4 , byte_mat4);
-        buff_vec_mat4_.emplace_back(
-                                        mat4[0], mat4[1], mat4[2], mat4[3], 
-                                        mat4[4], mat4[5], mat4[6], mat4[7],
-                                        mat4[8], mat4[9], mat4[10], mat4[11],
-                                        mat4[12], mat4[13], mat4[14], mat4[15]
-                                    );
+        size_t size_of_mat4 = i * 72;
+        std::copy(buff_scene_.begin() + 10 + size_of_mat4, buff_scene_.begin() + 18 + size_of_mat4, id_in_bytes);
+        std::copy(buff_scene_.begin() + 18 + size_of_mat4, buff_scene_.begin() + 72 + size_of_mat4 , byte_mat4);
+        const size_t* id = reinterpret_cast<const size_t*>(id_in_bytes);
+        if(buff_map_mat4_.end() == buff_map_mat4_.find(*id)) {
+            buff_map_mat4_.emplace( std::make_pair(*id, Ogre::Matrix4( 
+                                            mat4[0], mat4[1], mat4[2], mat4[3], 
+                                            mat4[4], mat4[5], mat4[6], mat4[7],
+                                            mat4[8], mat4[9], mat4[10], mat4[11],
+                                            mat4[12], mat4[13], mat4[14], mat4[15])
+            ));
+        } else {
+            buff_map_mat4_.at(*id) = std::move(Ogre::Matrix4( 
+                                            mat4[0], mat4[1], mat4[2], mat4[3], 
+                                            mat4[4], mat4[5], mat4[6], mat4[7],
+                                            mat4[8], mat4[9], mat4[10], mat4[11],
+                                            mat4[12], mat4[13], mat4[14], mat4[15]));
+        }
     }
 }
 
@@ -38,20 +49,20 @@ void MeshObject::create_scene_nodes() {
    
     Ogre::SceneManager* sceneManager = mPtrSituationMonitor_->getSceneManagerS();
     
-    for(size_t i = 0; i < buff_vec_mat4_.size();) {
-        const Ogre::Affine3 affine3(buff_vec_mat4_[i]);
+    for(auto& sn_obj : buff_map_mat4_) {
+        const Ogre::Affine3 affine3(sn_obj.second);
         Vector3 position; 
         Vector3 scale; 
         Quaternion orientation;
         
         affine3.decomposition(position, scale, orientation);
-        if(i == 0) {
+        if(sn_obj.first == 0) {
             own_position_ = position;
         }
         else {
             Vector3 curr_position = position - own_position_;
-            Ogre::Entity* unitCubeEnt = sceneManager->createEntity( "CubeObserver.mesh");
-            Ogre::SceneNode* sceneNode = sceneManager->getRootSceneNode()->createChildSceneNode();
+            //Ogre::Entity* unitCubeEnt = sceneManager->createEntity( "CubeObserver.mesh");
+            //Ogre::SceneNode* sceneNode = sceneManager->getRootSceneNode()->createChildSceneNode();
             
         }
         
